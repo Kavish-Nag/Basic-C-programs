ca8c836a1a1d7f5f94a331ec8b507f2697cbd5ae
Kavish-Nag <kavishnag13@gmail.com>
2025-03-17 15:10:48 +0530

    Add files via upload

---
+++ pattern.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+
+    for(int i=1;i<=4;i++){
+        printf("***** \n");
+        // for(int i=1;i<=5;i++){
+        //     printf("*****");
+        // }    
+
+
+    }
+    return 0;
+}
